bf2730e4fca1beb4faf244a3fe16860f3ca3fc82
Hamza <haamzaasaleem@gmail.com>
2024-09-06 00:58:46 +0500

    FIXED CORS

--- core/settings.py
+++ core/settings.py
@@ -186,6 +186,13 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS=True
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+   ]
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
