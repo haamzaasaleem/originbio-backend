322e71914663679ad31019ee3bb0723c83e6e537
Hamza <haamzaasaleem@gmail.com>
2024-09-06 00:53:06 +0500

    fixed CORS

--- core/settings.py
+++ core/settings.py
@@ -186,4 +186,10 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS=True
 
-# CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+]
